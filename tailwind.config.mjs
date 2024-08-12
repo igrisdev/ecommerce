bd18706656d98ca35b1895b84a339ab9419e5924
igrisdev <johanalvarezz237@gmail.com>
2024-08-11 22:33:33 -0500

    actualizado: colores de tailwindcss y los alias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        local_background_primary: '#FFFFFF',
+        local_background_secondary: '#121212',
+        local_background_tertiary: '#EFEFEF',
+        // local_background_quaternary: '#00A082',
+        local_title_primary: '#121212',
+        local_title_secondary: '#EFEFEF',
+        local_title_tertiary: '#000000',
+        // local_title_quaternary: '#FFFFFF',
+        local_subtitle_primary: '#121212',
+        local_subtitle_secondary: '#EFEFEF',
+        local_text_primary: '#121212',
+        local_text_secondary: '#EFEFEF',
+        // local_text_tertiary: '#d4d4d4',
+        // local_border_primary: '#E7E7E7',
+        local_accent_primary: '#E75A55',
+        local_icons_primary: '#121212',
+        local_icons_secondary: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
 }
